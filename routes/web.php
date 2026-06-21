f9c46aaa238afe4e63c0a05f949e151ae2d213e0
unknown <mangulabnanshervinmarco@gmail.com>
2026-06-21 18:48:51 +0800

    fix errors

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,6 @@ Route::post('/tasks', [TaskController::class, 'store']);
 
 Route::put('/tasks/{task}', [TaskController::class, 'update']);
 
+Route::put('/tasks/{task}/status', [TaskController::class, 'updateStatus']);
+
 Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
