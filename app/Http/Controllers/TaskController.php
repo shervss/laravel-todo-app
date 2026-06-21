b9a14b06155fa9d4cd25cb4deb4c48e68da31900
unknown <mangulabnanshervinmarco@gmail.com>
2026-06-21 18:05:09 +0800

    Implement task controller CRUD functionality

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index()
+    {
+        $tasks = Task::latest()->get();
+
+        return view('tasks.index', compact('tasks'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required'
+        ]);
+
+        Task::create([
+            'title' => $request->title
+        ]);
+
+        return redirect('/');
+    }
+
+    public function update(Task $task)
+    {
+        $task->update([
+            'completed' => !$task->completed
+        ]);
+
+        return redirect('/');
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return redirect('/');
+    }
 }
